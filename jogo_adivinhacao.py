b50113fb0f63da15e333b2f24e3f1af278ae3cd8
unknown <wndydev@gmail.com>
2022-11-28 15:48:37 -0300

    Sistema de pontos e dificuldade (Jogo terminado)

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -5,11 +5,22 @@ print("Bem-vindo ao jogo de adivinhação");
 print("**********************************");
 
 secret_number = random.randint(1,100); # Sorteando um número
-maximum_attempts = 3; # Número máximo de tentativas
+maximum_attempts = 0; # Número máximo de tentativas
 current_atempt = 1; # Tentativa atual
+score = 1000; # pontuação
+
+print("Qual o nível de dificuldade desejado?");
+print("(1) fácil, (2) Padrão, (3) Difícil");
+difficulty = int(input("Escolha a dificuldade: "));
+
+if(difficulty == 1):
+    maximum_attempts = 10;
+elif(difficulty == 2):
+    maximum_attempts = 5;
+elif(difficulty == 3):
+    maximum_attempts = 3;
 
 while(current_atempt <= maximum_attempts):
-    print(secret_number);
     print("Tentativa atual: {} de {}".format(current_atempt,maximum_attempts));
     choice = int(input("Digite um número de 1 a 100: ")); # Transformando o input "String" para "Int"
     print("Você digitou o número" , choice);
@@ -21,13 +32,15 @@ while(current_atempt <= maximum_attempts):
 
 
     if(choice == secret_number): 
-        print("Você acertou :)");
+        print("Você acertou e fez {} pontos, parabéns".format(score));
         break;
     else:
         if(choice < secret_number):
             print("O número que você digitou é menor do que o número sorteado");
         if(choice > secret_number):
             print("O número que você digitou é maior do que o número sorteado");
+        lost_score = abs(secret_number - choice);
+        score -= lost_score;
     current_atempt += 1;
 
 print("********************************************************************");
