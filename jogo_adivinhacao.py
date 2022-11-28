f6457c7a0014576a2005cc7369507d771eae41cc
unknown <wndydev@gmail.com>
2022-11-28 14:10:03 -0300

    Limitando os números que podem ser jogados

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -2,23 +2,31 @@ print("**********************************");
 print("Bem-vindo ao jogo de adivinhação");
 print("**********************************");
 
-secret_number = 42;
-maximum_attempts = 3;
-current_tempt = 1;
+secret_number = 42; # Número sorteado
+maximum_attempts = 3; # Número máximo de tentativas
+current_atempt = 1; # Tentativa atual
 
-while(current_tempt <= maximum_attempts):
-    choice = int(input("Digite o seu número: ")); # Transformando o input "String" para "Int"
+while(current_atempt <= maximum_attempts):
+
+    print("Tentativa atual: {} de {}".format(current_atempt,maximum_attempts));
+    choice = int(input("Digite um número de 1 a 100: ")); # Transformando o input "String" para "Int"
     print("Você digitou o número" , choice);
+
+
+    if(choice < 1 or choice > 100):
+        print("Por favor, digite um número entre 1 e 100");
+        continue;
+
+
     if(choice == secret_number): 
         print("Você acertou :)");
         break;
     else:
         if(choice < secret_number):
             print("O número que você digitou é menor do que o número sorteado");
-            current_tempt += 1;
         if(choice > secret_number):
             print("O número que você digitou é maior do que o número sorteado");
-            current_tempt += 1;
+    current_atempt += 1;
 
 print("********************************************************************");
 print("FIM DE JOGO (APERTE F5 PARA JOGAR NOVAMENTE");
