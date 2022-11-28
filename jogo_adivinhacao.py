d778de936c53a4e56368cc7bb815c5d00f24f323
unknown <wndydev@gmail.com>
2022-11-28 10:46:32 -0300

    Criando 3 tentativas para o usuário

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -3,18 +3,22 @@ print("Bem-vindo ao jogo de adivinhação");
 print("**********************************");
 
 secret_number = 42;
+maximum_attempts = 3;
+current_tempt = 1;
 
-choice = int(input("Digite o seu número: ")); # Transformando o input "String" para "Int"
-
-print("Você digitou o número" , choice);
-
-if(choice == secret_number): 
-    print("Você acertou :)");
-else:
-    if(choice < secret_number):
-        print("O número que você digitou é menor do que o número sorteado");
-    if(choice > secret_number):
-        print("O número que você digitou é maior do que o número sorteado");
+while(current_tempt <= maximum_attempts):
+    choice = int(input("Digite o seu número: ")); # Transformando o input "String" para "Int"
+    print("Você digitou o número" , choice);
+    if(choice == secret_number): 
+        print("Você acertou :)");
+        break;
+    else:
+        if(choice < secret_number):
+            print("O número que você digitou é menor do que o número sorteado");
+            current_tempt += 1;
+        if(choice > secret_number):
+            print("O número que você digitou é maior do que o número sorteado");
+            current_tempt += 1;
 
 print("********************************************************************");
 print("FIM DE JOGO (APERTE F5 PARA JOGAR NOVAMENTE");
